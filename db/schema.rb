419e3be8e16f27c6ff9c75a600456050dc2eb4a6
tgustafson2 <gus.duckfan@gmail.com>
2023-11-12 17:23:37 -0800

    Finish user signup

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_12_040255) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
