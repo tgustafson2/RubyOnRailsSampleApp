4b8e8c11aab10619b4dab342f00ae8738cce5213
tgustafson2 <gus.duckfan@gmail.com>
2023-11-14 15:15:29 -0800

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,3 +19,12 @@ module ActiveSupport
     end
   end
 end
+
+
+module ActionDispatch
+  class IntegrationTest
+    def log_in_as(user, password: 'password', remember_me: '1')
+      post login_path, params: {session: {email: user.email, password: password, remember_me: remember_me}}
+    end
+  end
+end
